93ff18c2658c06313e14a92f1cfd703d58ba4585
Krishanrx8 <krishanrx8@gmail.com>
2022-09-24 22:32:16 +0530

    login updated

---
+++ DMT_ExaminationSystem/ClientApp/src/components/Login.js
@@ -0,0 +1,32 @@
+﻿import React, { Component } from 'react';
+import './Login.css';
+
+export class Login extends Component {
+    static displayName = Login.name;
+
+    render() {
+        return (
+            <div>
+            <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
+    
+        <h3>Login Here</h3>
+
+        <label for="username">Username</label>
+        <input type="text" placeholder="Email or Phone" id="username"/>
+
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" id="password"/>
+
+        <button>Log In</button>
+        <div class="social">
+          <div class="go"><i class="fab fa-google"></i>  Google</div>
+          <div class="fb"><i class="fab fa-facebook"></i>  Facebook</div>
+        </div>
+                        
+                    </div>
+        );
+    }
+}
--- DMT_ExaminationSystem/ClientApp/src/components/NavMenu.js
+++ DMT_ExaminationSystem/ClientApp/src/components/NavMenu.js
@@ -37,7 +37,10 @@ export class NavMenu extends Component {
               </NavItem>
               <NavItem>
                 <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-              </NavItem>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} className="text-dark" to="/login">Login</NavLink>
+                        </NavItem>
             </ul>
           </Collapse>
         </Navbar>
